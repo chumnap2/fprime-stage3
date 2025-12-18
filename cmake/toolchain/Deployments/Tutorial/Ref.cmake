0ce05b6e67d844eb970f30fa6e4bb5f9cf227699
chumnap2 <thachchumnap@gmail.com>
2025-12-18 19:40:57 +1300

    Add minimal toolchain for Tutorial/Ref deployment; verified working with GDS

---
+++ cmake/toolchain/Deployments/Tutorial/Ref.cmake
@@ -0,0 +1,7 @@
+# Minimal toolchain for Tutorial/Ref (host Linux build)
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+set(FPRIME_PLATFORM Linux)
+set(FPRIME_OS Linux)
